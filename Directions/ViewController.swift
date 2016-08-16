3055106d2409718ba017d3a19d240744ad3d8d83
Javier Hernández Pineda <javier.hernandez@la-macbook-air-de-javier.local>
2016-08-16 16:07:01 -0500

    Initial Commit

---
+++ Directions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Directions
+//
+//  Created by Javier Hernández Pineda on 16/08/16.
+//  Copyright © 2016 Javier Hernández Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
